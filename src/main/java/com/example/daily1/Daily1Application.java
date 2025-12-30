cf3b4c0c7a22b95a5632d2b34ac400dc568c16b5
youzting <158013348+youzting@users.noreply.github.com>
2025-12-30 11:18:00 +0900

    readme API명세서 작성

---
+++ src/main/java/com/example/daily1/Daily1Application.java
@@ -0,0 +1,13 @@
+package com.example.daily1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daily1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Daily1Application.class, args);
+    }
+
+}
